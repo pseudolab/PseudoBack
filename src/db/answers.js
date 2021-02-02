907577611bb840a9b01c6e08e60d701f6f236dd6
ssilb4 <ssilb4@gmail.com>
2021-02-02 21:42:38 +0900

    add qna and reply

---
+++ src/db/answers.js
@@ -0,0 +1,54 @@
+const Joi = require('joi');
+const db = require('./connection');
+const fs = require('fs');
+const counter = require('./counter');
+ 
+const schema = Joi.object().keys({
+    username: Joi.string().alphanum().required(),
+    subject: Joi.string().required(),
+    content: Joi.string().max(500).required(),
+    postID: Joi.number().required(),
+});
+ 
+const answers = db.get('answers');
+ 
+function get(id) {
+    return answers.find({"postID":id});
+}
+ 
+async function create(answer) {
+    if (!answer.username) answer.username = 'Anonymous';
+
+    const result = schema.validate(answer);
+    if (result.error == null) {
+        const fileDir =  './content/';
+        const d = new Date();
+        const date = d.getFullYear() + ("0" + (d.getMonth()+1)).slice(-2) + ("0" + d.getDate()).slice(-2)  + ("0" + d.getHours()).slice(-2)  + ("0" + d.getMinutes()).slice(-2)  + ("0" + d.getSeconds()).slice(-2);
+        answer.created = date;
+
+        /* todo: 향후 username 대신 userid로 변경 필요 */
+        const contents = answer.content;
+        filename = answer['username'] + date;
+        answer.content = fileDir + filename;
+        
+        let stream = fs.createWriteStream(fileDir + filename);
+        stream.once('open', function(fd) {
+            stream.write(contents);
+            stream.end();
+        });
+
+        //const count = await counter.getCounter("answers");
+        //answe.postID = count;
+        //console.log("testset" + count);
+        
+        return answers.insert(answer);
+    } else {
+        console.log('error');
+        return Promise.reject(result.error);
+    }
+}
+ 
+module.exports = {
+    create,
+    get
+};;
---
+++ src/routes/posts/posts.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const posts = require('@db/posts');
+
+/**
+* BaseUrl : web.js router에 선언한 BaseUrl을 표시. request url을 쉽게 파악하기 위함
+*  : /club
+*/
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+
+router.use(morgan('tiny'));
+router.use(cors());
+router.use(bodyParser.json());
+
+router.get('/', (req, res) => {
+    posts.getAll().then((post) => {
+        const fs = require('fs');
+        res.json(post);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const postID = Number(req.params.id);
+    posts.get(postID).then((post) => {
+        const fs = require('fs');
+        console.log(post);
+        const filename = post['content'];
+        try{
+            const content = fs.readFileSync(filename).toString();
+            post['content'] = content;
+        } catch(error){
+        }
+        res.json(post);
+    });
+});
+
+router.post('/', (req, res) => {
+    posts.create(req.body).then((post) => {
+        const fs = require('fs');
+
+        const filename = post['content'];
+        try{
+            const content = fs.readFileSync(filename).toString();
+            post['content'] = content;
+        } catch(error){
+            console.log(error);
+        }
+        res.json(post);
+    }).catch((error) => {
+        res.status(500);
+        res.json(error);
+    });
+});
+
+
+
+module.exports = router;
