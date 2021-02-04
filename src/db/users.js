bd9c1df2e1dc9c5031c4b53d098deaeb9d2b7b96
epikem <epikem1@gmail.com>
2021-02-04 10:14:08 +0900

    update user schema

--- src/db/users.js
+++ src/db/users.js
@@ -1,8 +1,25 @@
 const Joi = require('joi');
 const db = require('./connection');
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
 
 const schema = Joi.object().keys({
-    userID: Joi.number().required(),
+    id: Joi.string().required(),
     userName: Joi.string().alphanum().min(3).max(40).required(),
     password: Joi.string()
         .pattern(new RegExp('^[a-zA-Z0-9]{5,30}$')).required(),
@@ -16,6 +33,12 @@ const schema = Joi.object().keys({
     description: Joi.string().max(300),
     numBoard: Joi.number(),
     numReply: Joi.number(),
+    social: {
+      google: Joi.object().keys({
+        displayName: Joi.string(),
+        emails: Joi.string()
+      })
+    },
     profileImageURL: Joi.string().uri({
         scheme: [
             /https?/
@@ -31,7 +54,7 @@ function getAll() {
 
 function get(userID){
     return users.findOne({
-        userID
+        id: userID
     });
 }
 
@@ -53,7 +76,34 @@ function create(user) {
         return Promise.reject(result.error);
     }
 }
- 
+
+passport.serializeUser((user,done)=>{
+  console.log('serialize start!')
+  console.log(user)
+  user.password='password123'
+  user.repeatPassword='password123'
+  create(user).then((res)=>{
+    console.log('SERIALIZE RESULT')
+    console.info(res.id)
+    done(null, user.id)
+  }).catch((err)=>{
+    console.error(err)
+    throw err
+  })
+})
+
+passport.deserializeUser((id, done)=>{
+  console.log('try desrialize!')
+  users.findOne({
+    id: id
+  }, (err, user)=>{
+    console.log('DESRIALAREUSULT')
+    console.warn(user)
+    done(err, user)
+  })
+
+})
+
 module.exports = {
     create,
     getAll,
