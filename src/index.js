ca7ebb01f06a97309cccb255c81521a4409f0e4c
epikem <epikem1@gmail.com>
2021-02-04 10:10:41 +0900

    use auth packages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require('module-alias/register');
 
 const express = require('express');
+const session = require('express-session');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const morgan = require('morgan');
 const http = require('http');
 
@@ -12,7 +15,20 @@ const routes = require('./routes');
  
 app.use(morgan('tiny'));
 app.use(cors());
+app.use(cookieParser());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({ 
+    secret: 'keycat cat',
+    resave: false,
+    saveUninitialized: false
+}));
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/routes', routes)
 
