e84a07223e3d1d620395e349cccf0ba12a779622
ssilb4 <ssilb4@gmail.com>
2021-06-16 20:38:53 +0900

    add category description

--- src/db/categories.js
+++ src/db/categories.js
@@ -3,30 +3,36 @@ const db = require('./connection');
  
 const schema = Joi.object().keys({
     categoryName: Joi.string().required(),
+    description: Joi.string().required(),
     cowriter: Joi.array().items(Joi.string().alphanum())
 });
  
-const posts = db.get('categories');
+const categories = db.get('categories');
  
 function getAll() {
-    return posts.find();
+    return categories.find();
 }
 
 function get(name) {
-    return posts.findOne({"categoryName":name});
+    return categories.findOne({"categoryName":name});
 }
  
 function create(category) {
     const result = schema.validate(category);
-    return posts.insert(category);
+    if (result.error == null) {
+        return categories.insert(category);
+    } else {
+        console.log('error');
+        return Promise.reject(result.error);
+    }
 }
 
 function update(name, cowriter) {
-    return posts.update({"categoryName":name}, { $set: {"cowriter": cowriter}});
+    return categories.update({"categoryName":name}, { $set: {"cowriter": cowriter}});
 }
 
 function remove(name) {
-    return posts.remove({"categoryName":name});
+    return categories.remove({"categoryName":name});
 }
 
 module.exports = {
