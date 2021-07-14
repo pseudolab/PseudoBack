2d4f3104500500fc664d04349aaa81d3ec4612de
epikem <epikem@kakao.com>
2021-07-14 21:50:35 +0900

    add dev route for user creation - remove dev route code from routes index

--- src/routes/dev/index.js
+++ src/routes/dev/index.js
@@ -1,13 +1,43 @@
 const dev = require('express').Router();
 const db = require('@db/users');
 
+// router.get('/dev/:command', async (req, res) => {
+//   const command = req.params.command;
+  
+//   switch (command) {
+//     case 'drop':
+//       db.dropAll();
+//       break;
+  
+//     default:
+//       break;
+//   }
+//   res.sendStatus(200);
+// });
+
 dev.get('/check', (req, res) => {
   console.info('check call');
   res.sendStatus(200);
 })
 
+dev.get('/create-user', async (req,res)=>{
+  console.warn('create new user');
+  await db.dropAll();
+  const response = await db.create({
+    provider: 'google',
+    id: 'uid1',
+    userMail: 'asd@asd.com',
+    userID: 'uid2',
+    userName: 'Epikem'
+  });
+
+  console.log(response);
+
+  res.status(200).json(response);
+})
+
 dev.get('/reset', async (req,res)=>{
-  console.log('delete all');
+  console.warn('DROP ALL USERS');
   
   await db.dropAll();
   res.sendStatus(203);
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const usersRouter = require('./users');
 const qnasRouter = require('./qnas');
 const pofilesRouter = require('./profiles');
 const categoriesRouter = require('./categories');
+const devRouter = require('./dev');
 
 //request 요청 URL과 처리 로직을 선언한 라우팅 모듈 매핑
 router.use('/posts', postsRouter);
@@ -18,21 +19,7 @@ router.use('/categories', categoriesRouter);
 
 if(process.env.NODE_ENV === 'development') {
   console.warn('DEVELOPMENT ROUTE ACTIVATED')
-  const db = require('@db/users');
-  router.get('/dev/:command', async (req, res) => {
-    const command = req.params.command;
-    
-    switch (command) {
-      case 'drop':
-        console.warn('DROP ALL USERS');
-        db.dropAll();
-        break;
-    
-      default:
-        break;
-    }
-    res.sendStatus(200);
-  });
+  router.use('/dev', devRouter);
 }
 
 // Add more routes here if you want!
