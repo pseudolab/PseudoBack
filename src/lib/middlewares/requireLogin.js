803e3cf5885e0f47b5a03e76f2bd25a1ee4a3c48
epikem <epikem@kakao.com>
2021-07-28 23:29:58 +0900

    fix first login - fix first login not working correctly

--- src/lib/middlewares/requireLogin.js
+++ src/lib/middlewares/requireLogin.js
@@ -4,19 +4,10 @@ const users = require('@db/users');
 
 async function requireLogin(req, res, next) {
   try {
-    // const token = req.query.id_token;
     const token = req.get('auth-token');
-    // get user id
     const userInfo = await social.getGoogleUserInfo(token);
-    // check user exists
     const user = await users.get(userInfo.userid);
     if(!user) {
-      console.info('no user registered for this userid')
-
-      // // register 리다이렉트
-      // res.redirect('http://localhost:3000/register');
-      // return;
-
       const createdUser = await users.create({
         provider: 'google',
         userID: userInfo.userid,
@@ -25,11 +16,10 @@ async function requireLogin(req, res, next) {
         photo: userInfo.photo
       })
       
-      // res.json(createdUser);
-      // return;
       req.user = createdUser;
+    } else {
+      req.user = user;
     }
-    req.user = user;
     next();
   } catch (error) {
     // authentication fail
