62f138861447321380cd06b585192323bfdb7528
epikem <epikem@kakao.com>
2021-08-04 19:18:58 +0900

    serve images on static route

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ app.use(cors());
 
 app.use(['/routes', '/api/routes'], routes)
 app.use('/static', express.static(path.join(__dirname, 'dist')))
+app.use('/image', express.static(path.join(__dirname, '../images')))
 
 app.set('port', process.env.PORT || 4000);
 
