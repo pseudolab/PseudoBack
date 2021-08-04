0e84048ed1800009e8bd0dcc022ae0dec1bb964c
epikem <epikem@kakao.com>
2021-08-04 18:55:55 +0900

    add test login

--- src/lib/middlewares/requireLogin.js
+++ src/lib/middlewares/requireLogin.js
@@ -1,8 +1,23 @@
 // https://stackoverflow.com/questions/31063153/how-to-perform-login-redirect-with-nodejs-expressjs
 const social = require('../social');
 const users = require('@db/users');
+require('dotenv').config();
 
 async function requireLogin(req, res, next) {
+  if(process.env.NODE_ENV === 'development' && process.env.USE_FAKE_LOGIN) {
+    const createdUser = await users.create({
+      provider: 'google',
+      userID: 'fakeuserid',
+      userMail: 'fakeuser@pseudo.com',
+      userName: 'fakeuser-name',
+      photo: 'http://fakeuser.photo.url'
+    })
+    
+    req.user = createdUser;
+    next();
+    return;
+  }
+
   try {
     const token = req.get('auth-token');
     const userInfo = await social.getGoogleUserInfo(token);
