9f498f9cd5923cf379abd0277802dfc317ebee5d
epikem <epikem@kakao.com>
2021-08-20 18:28:11 +0900

    add db connection error check

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,4 +2,9 @@ const monk = require('monk');
 const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/';
 const db = monk(connectionString);
 
+db.catch(function(err) {
+  console.error(err);
+  console.error('DB CONNECTION FAILED');
+});
+
 module.exports = db;
